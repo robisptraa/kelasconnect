68fa1242853b367b765827b68174d916f42b9838
robisptraa <141976584+robisptraa@users.noreply.github.com>
2024-09-22 01:55:47 +0700

    build register pages

--- lib/pages/Register.dart
+++ lib/pages/Register.dart
@@ -20,59 +20,97 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     final respon = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.purple,
-      body: Stack(
-        children: [
-          Positioned(
-            top: respon.height * 0.08,
-            left: respon.width * 0.25,
-            child: Container(
-              height: respon.height * 0.16,
-              width: respon.width * 0.5,
-              decoration: BoxDecoration(
-                // color: Colors.black,
-                image: DecorationImage(
-                  image: AssetImage('assets/logo1.png'),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.purple,
+              Colors.pinkAccent,
+            ],
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: respon.height * 0.08,
+              left: respon.width * 0.25,
+              child: Container(
+                height: respon.height * 0.16,
+                width: respon.width * 0.5,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/logo1.png'),
+                  ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            top: respon.height * 0.25,
-            child: Container(
-              height: respon.height * 0.70,
-              width: respon.width,
-              decoration: BoxDecoration(
+            Positioned(
+              top: respon.height * 0.25,
+              child: Container(
+                width: respon.width,
+                decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius:
-                      BorderRadius.all(Radius.circular(respon.width * 0.08))),
-              child: Column(
-                children: [
-                  SizedBox(height: respon.height * 0.05),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [Image.asset('assets/SignUP.png')],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(respon.width * 0.08),
                   ),
-                  Padding(
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
                     padding: EdgeInsets.symmetric(
                         horizontal: respon.width * 0.05,
-                        vertical: respon.height * 0.055),
+                        vertical: respon.height * 0.03),
                     child: Column(
                       children: [
+                        SizedBox(height: respon.height * 0.03),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [Image.asset('assets/SignUP.png')],
+                        ),
+                        SizedBox(height: respon.height * 0.04),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.person),
+                                  label: Text("First name"),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        respon.width * 0.025),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: respon.width * 0.03),
+                            Expanded(
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.person_2_outlined),
+                                  label: Text("Last name"),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        respon.width * 0.025),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: respon.height * 0.02),
                         TextField(
                           keyboardType: TextInputType.emailAddress,
                           decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.person),
-                            label: Text("emailadress"),
+                            prefixIcon: Icon(Icons.email),
+                            label: Text("E-Mail Address"),
                             border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.circular(respon.width * 0.05),
+                              borderRadius: BorderRadius.circular(
+                                  respon.width * 0.05),
                             ),
                           ),
                         ),
-                        SizedBox(
-                          height: respon.height * 0.02,
-                        ),
+                        SizedBox(height: respon.height * 0.02),
                         TextField(
                           keyboardType: TextInputType.visiblePassword,
                           obscureText: _hide,
@@ -85,49 +123,88 @@ class _RegisterState extends State<Register> {
                             ),
                             label: Text("password"),
                             border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.circular(respon.width * 0.05),
+                              borderRadius: BorderRadius.circular(
+                                  respon.width * 0.05),
                             ),
                           ),
                         ),
-                        SizedBox(height: respon.height * 0.01),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size(double.infinity, 50),
-                            backgroundColor: Colors.purple,
+                        SizedBox(height: respon.height * 0.02),
+                        TextField(
+                          keyboardType: TextInputType.visiblePassword,
+                          obscureText: _hide,
+                          decoration: InputDecoration(
+                            prefixIcon: IconButton(
+                              onPressed: _hidepw,
+                              icon: Icon(_hide
+                                  ? Icons.remove_red_eye
+                                  : Icons.remove_red_eye_outlined),
+                            ),
+                            label: Text("Confirm password"),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(
+                                  respon.width * 0.05),
+                            ),
                           ),
-                          child: Text('Sign Up',
-                              style: TextStyle(
+                        ),
+                        SizedBox(height: respon.height * 0.03),
+                        Container(
+                          width: double.infinity,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Colors.purple, Colors.pink],
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: InkWell(
+                            onTap: () {
+                              // Logic untuk login
+                            },
+                            child: Center(
+                              child: Text(
+                                'Sign Up',
+                                style: TextStyle(
                                   color: Colors.white,
                                   fontWeight: FontWeight.bold,
-                                  fontSize: respon.width * 0.04)),
+                                  fontSize: respon.width * 0.04,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: respon.height * 0.02),
+                        Text(
+                          'or sign up with',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                            fontSize: respon.width * 0.04,
+                          ),
+                        ),
+                        SizedBox(height: respon.height * 0.02),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/Google,i.png', width: 50),
+                            SizedBox(width: respon.width * 0.05),
+                            Image.asset('assets/Facebook.png', width: 50),
+                          ],
+                        ),
+                        SizedBox(height: respon.height * 0.01),
+                        Text(
+                          '© 2024 Robet,reiizuko',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
                       ],
                     ),
                   ),
-                  Text(
-                    'or sign up with',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w600,
-                        fontSize: respon.width * 0.03),
-                  ),
-                  SizedBox(height: respon.height * 0.01),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/Google,i.png'),
-                      Image.asset('assets/Facebook.png'),
-                    ],
-                  ),
-                  SizedBox(height: respon.height * 0.01,),
-                  Text('© 2024 Robet,reiizuko', style: TextStyle(fontWeight: FontWeight.w400),),
-                ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
